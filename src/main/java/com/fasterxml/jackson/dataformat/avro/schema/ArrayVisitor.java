165070fd439c8ae0af06cb527a26be409db6a836
Tatu Saloranta <tsaloranta@gmail.com>
2012-11-19 20:51:22 -0800

    ...

--- src/main/java/com/fasterxml/jackson/dataformat/avro/schema/ArrayVisitor.java
+++ src/main/java/com/fasterxml/jackson/dataformat/avro/schema/ArrayVisitor.java
@@ -2,11 +2,10 @@ package com.fasterxml.jackson.dataformat.avro.schema;
 
 import org.apache.avro.Schema;
 
-import java.util.*;
-
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonArrayFormatVisitor;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitable;
 
 public class ArrayVisitor
     extends VisitorBase
@@ -16,29 +15,20 @@ public class ArrayVisitor
     
     protected final DefinedSchemas _schemas;
 
-    protected Schema _avroSchema;
-    
-    protected List<Schema.Field> _fields = new ArrayList<Schema.Field>();
+    protected Schema _elementSchema;
     
     public ArrayVisitor(JavaType type, DefinedSchemas schemas)
     {
         _type = type;
         _schemas = schemas;
-        Class<?> cls = type.getRawClass();
-        String name = cls.getSimpleName();
-        Package pkg = cls.getPackage();
-        String namespace = (pkg == null) ? "" : pkg.getName();
-        _avroSchema = Schema.createRecord(name,
-                "Schema for "+type,
-                namespace, false);
-        schemas.addSchema(type, _avroSchema);
     }
     
     @Override
     public Schema getAvroSchema() {
-        // Assumption now is that we are done, so let's assign fields
-        _avroSchema.setFields(_fields);
-        return _avroSchema;
+        if (_elementSchema == null) {
+            throw new IllegalStateException("No element schema created for: "+_type);
+        }
+        return Schema.createArray(_elementSchema);
     }
 
     /*
@@ -48,20 +38,17 @@ public class ArrayVisitor
      */
 
     @Override
-    public void itemsFormat(JavaType type) throws JsonMappingException {
-        // TODO Auto-generated method stub
-        
+    public void itemsFormat(JsonFormatVisitable visitable, JavaType type)
+            throws JsonMappingException
+    {
+        VisitorFormatWrapperImpl wrapper = new VisitorFormatWrapperImpl(_schemas);
+        visitable.acceptJsonFormatVisitor(wrapper, type);
+        _elementSchema = wrapper.getGeneratedSchema();
     }
 
     @Override
-    public void itemsFormat(JsonFormatTypes type) throws JsonMappingException {
-        // TODO Auto-generated method stub
-        
+    public void itemsFormat(JsonFormatTypes type) throws JsonMappingException
+    {
+        _elementSchema = simpleSchema(type);
     }
-    
-    /*
-    /**********************************************************************
-    /* Internal methods
-    /**********************************************************************
-     */
 }
--- src/main/java/com/fasterxml/jackson/dataformat/avro/schema/VisitorBase.java
+++ src/main/java/com/fasterxml/jackson/dataformat/avro/schema/VisitorBase.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.avro.Schema;
 
 import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatVisitorWithSerializerProvider;
 
 public abstract class VisitorBase
@@ -39,6 +40,28 @@ public abstract class VisitorBase
         return Schema.createUnion(schemas);
     }
 
+    public Schema simpleSchema(JsonFormatTypes type)
+    {
+        switch (type) {
+        case BOOLEAN:
+            return Schema.create(Schema.Type.BOOLEAN);
+        case INTEGER:
+            return Schema.create(Schema.Type.INT);
+        case NULL:
+            return Schema.create(Schema.Type.NULL);
+        case NUMBER:
+            return Schema.create(Schema.Type.DOUBLE);
+        case STRING:
+            return Schema.create(Schema.Type.STRING);
+        case ARRAY:
+        case OBJECT:
+            throw new UnsupportedOperationException("Should not try to create simple Schema for: "+type);
+        case ANY: // might be able to support in future
+        default:
+            throw new UnsupportedOperationException("Can not create Schema for: "+type+"; not (yet) supported");
+        }
+    }
+    
     protected <T> T _throwUnsupported() {
         throw new UnsupportedOperationException("Format variation not supported");
     }
