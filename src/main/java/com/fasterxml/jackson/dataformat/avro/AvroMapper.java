07280d7b624e654f10d4872f82e692f99a3b0f60
Tatu Saloranta <tatu.saloranta@iki.fi>
2014-10-07 23:02:26 -0700

    Implement #11

--- src/main/java/com/fasterxml/jackson/dataformat/avro/AvroMapper.java
+++ src/main/java/com/fasterxml/jackson/dataformat/avro/AvroMapper.java
@@ -1,11 +1,15 @@
 package com.fasterxml.jackson.dataformat.avro;
 
 import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.avro.schema.AvroSchemaGenerator;
 
 /**
  * Convenience {@link AvroMapper}, which is mostly similar to simply
- * constructing a mapper with {@link AvroFactory}.
+ * constructing a mapper with {@link AvroFactory}, but also adds little
+ * bit of convenience around {@link AvroSchema} generation.
  * 
  * @since 2.4
  */
@@ -35,5 +39,25 @@ public class AvroMapper extends ObjectMapper
     @Override
     public Version version() {
         return PackageVersion.VERSION;
+    }
+
+    /**
+     * @since 2.5
+     */
+    public AvroSchema schemaFor(Class<?> type) throws JsonMappingException
+    {
+    	AvroSchemaGenerator gen = new AvroSchemaGenerator();
+    	this.acceptJsonFormatVisitor(type, gen);
+    	return gen.getGeneratedSchema();
+    }
+
+    /**
+     * @since 2.5
+     */
+    public AvroSchema schemaFor(JavaType type) throws JsonMappingException
+    {
+    	AvroSchemaGenerator gen = new AvroSchemaGenerator();
+    	this.acceptJsonFormatVisitor(type, gen);
+    	return gen.getGeneratedSchema();
     }
 }
--- src/test/java/com/fasterxml/jackson/dataformat/avro/EnumTest.java
+++ src/test/java/com/fasterxml/jackson/dataformat/avro/EnumTest.java
@@ -1,7 +1,5 @@
 package com.fasterxml.jackson.dataformat.avro;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 public class EnumTest extends AvroTestBase
 {
     protected final static String ENUM_SCHEMA_JSON = "{\n"
@@ -19,19 +17,37 @@ public class EnumTest extends AvroTestBase
         public Gender gender;
     }
 
+    private final AvroMapper MAPPER = new AvroMapper();
+    
     public void testSimple() throws Exception
     {
-        ObjectMapper mapper = new ObjectMapper(new AvroFactory());
         AvroSchema schema = parseSchema(ENUM_SCHEMA_JSON);
         Employee input = new Employee();
         input.gender = Gender.F;
 
-        byte[] bytes = mapper.writer(schema).writeValueAsBytes(input);
+        byte[] bytes = MAPPER.writer(schema).writeValueAsBytes(input);
         assertNotNull(bytes);
         assertEquals(1, bytes.length); // measured to be current exp size
 
         // and then back
-        Employee output = mapper.reader(Employee.class).with(schema)
+        Employee output = MAPPER.reader(Employee.class).with(schema)
+                .readValue(bytes);
+        assertNotNull(output);
+        assertEquals(Gender.F, output.gender);
+    }
+
+    public void testEnumViaGeneratedSchema() throws Exception
+    {
+    	final AvroSchema schema = MAPPER.schemaFor(Employee.class);
+        Employee input = new Employee();
+        input.gender = Gender.F;
+    	
+        byte[] bytes = MAPPER.writer(schema).writeValueAsBytes(input);
+        assertNotNull(bytes);
+        assertEquals(1, bytes.length); // measured to be current exp size
+
+        // and then back
+        Employee output = MAPPER.reader(Employee.class).with(schema)
                 .readValue(bytes);
         assertNotNull(output);
         assertEquals(Gender.F, output.gender);
